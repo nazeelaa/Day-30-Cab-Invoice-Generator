10e2c816123d380c9408bb4532d26339abb0a99d
nazeelaa <92171051+nazeelaa@users.noreply.github.com>
2021-12-08 18:31:17 +0530

    CabInvoiceGenerator Program Pushed

---
+++ CabInvoiceGenerator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace CabInvoiceGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Cab Invoice Generator");
+            InvoiceGenerator invoiceGeneartor = new InvoiceGenerator(RideType.Normal);
+            double fare = invoiceGeneartor.CalcaulateFare(2.0, 5);
+            Console.WriteLine($"Fare : {fare}");
+
+            Ride[] rides = { new Ride(2.0, 5), new Ride(1.0, 1) };
+            invoiceGeneartor.AddRides("Nazeela", rides);
+
+            invoiceGeneartor.GetInvoiceSummary("Nazeela");
+        }
+    }
+}
